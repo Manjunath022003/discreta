5cf3d4d08adcd0f3d266bc523d9f3185f41d8b7a
Manjunath022003 <153350089+Manjunath022003@users.noreply.github.com>
2025-06-12 10:40:24 +0530

    Update AdminLogin.js

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -15,7 +15,7 @@ function AdminLogin() {
 
     // Send to backend for authentication
     try {
-      const response = await fetch("http://localhost:5000/admin-login", {
+      const response = await fetch("${process.env.REACT_APP_API_BASE_URL}/admin-login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(admin),
