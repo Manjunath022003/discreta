7e3209f421e2fd7fe2c2a16933141cced258d0e9
Manjunath022003 <153350089+Manjunath022003@users.noreply.github.com>
2025-06-12 12:09:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/discreta",{useNewUrlParser:true,useUnifiedTopology:true,})
+  .connect(process.env.MONGO_URI , {useNewUrlParser:true,useUnifiedTopology:true})
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
